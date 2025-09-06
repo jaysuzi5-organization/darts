df901b997f0cc90ae61143a679850214adacb173
Jay Curtis <jaysuzi5@gmail.com>
2025-09-06 16:56:58 -0400

    Increasing the limit to 1000

--- src/api/darts.py
+++ src/api/darts.py
@@ -22,7 +22,7 @@ def serialize_sqlalchemy_obj(obj):
 @router.get("/api/v1/darts")
 def list_darts(
     page: int = Query(1, ge=1, description="Page number to retrieve"),
-    limit: int = Query(10, ge=1, le=100, description="Number of records per page"),
+    limit: int = Query(10, ge=1, le=1000, description="Number of records per page"),
     db: Session = Depends(get_db)
 ):
     """
